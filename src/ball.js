0217723ecec802dd844f4ae008dc41b2ce527a7f
vitalii-8D <gotbubu0@gmail.com>
2020-12-08 04:59:28 +0200

    - Collision between the Ball and Paddle

--- src/ball.js
+++ src/ball.js
@@ -5,6 +5,8 @@ export default class Ball {
         this.gameWidth = game.gameWidth;
         this.gameHeight = game.gameHeight;
 
+        this.game = game;
+
         this.position = {x: 10, y: 10}
         this.speed = {x: 4, y: 2}
         this.size = 25;
@@ -18,12 +20,27 @@ export default class Ball {
         this.position.x += this.speed.x;
         this.position.y += this.speed.y;
 
+        // left and right wall
         if (this.position.x + this.size > this.gameWidth || this.position.x < 0) {
-            this.speed.x = - this.speed.x;
+            this.speed.x = -this.speed.x;
         }
 
+        // top or bottom
         if (this.position.y + this.size > this.gameHeight || this.position.y < 0) {
-            this.speed.y = - this.speed.y;
+            this.speed.y = -this.speed.y;
+        }
+
+        // check collisin with paddle
+        let bottomOfBall = this.position.y + this.size;
+        let topOfPaddle = this.game.paddle.position.y;
+        let leftSideOfPaddle = this.game.paddle.position.x;
+        let rightSideOfPaddle = this.game.paddle.position.x + this.game.paddle.width;
+
+        if (bottomOfBall >= topOfPaddle
+        && this.position.x >= leftSideOfPaddle
+        && this.position.x <= rightSideOfPaddle) {
+            this.speed.y = -this.speed.y;
+            this.position.y = this.game.paddle.position.y - this.size;
         }
     }
 }
