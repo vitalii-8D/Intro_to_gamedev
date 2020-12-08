b733bbd1af9cccc6f6e11d3d92dff7e5c8c2b792
vitalii-8D <gotbubu0@gmail.com>
2020-12-08 04:30:09 +0200

    - Refactoring to the Game Class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import Paddle from './paddle.js';
-import InputHandler from './input.js';
-import Ball from './ball.js';
+import Game from './game.js';
 
 let canvas = document.getElementById('gameScreen');
 let ctx = canvas.getContext('2d');
@@ -8,13 +6,8 @@ let ctx = canvas.getContext('2d');
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
-
-let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
-let ball = new Ball(GAME_WIDTH, GAME_HEIGHT);
-
-new InputHandler(paddle);
-
-paddle.draw(ctx);
+let game = new Game(GAME_WIDTH, GAME_HEIGHT);
+game.start();
 
 let lastTime = 0;
 
@@ -23,11 +16,9 @@ function gameLoop(timestamp) {
     lastTime = timestamp;
 
     ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-    paddle.update(deltaTime);
-    paddle.draw(ctx);
 
-    ball.update(deltaTime);
-    ball.draw(ctx);
+    game.update(deltaTime);
+    game.draw(ctx);
 
     requestAnimationFrame(gameLoop);
 }
